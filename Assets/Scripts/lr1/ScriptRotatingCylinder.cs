c47563a39ba37d030603b62a9c0d7b860f454d24
NOMOBHome\Nomob <tobenomob@gmail.com>
2021-04-13 11:21:04 +0300

    lr1

---
+++ Assets/Scripts/lr1/ScriptRotatingCylinder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptRotatingCylinder : MonoBehaviour
+{
+    [SerializeField] public new GameObject gameObject;
+    public Vector3 Direction;
+    public float MaxDistance = 5;
+
+    private Rigidbody _rigidbody;
+    private void Awake()
+    {
+        gameObject.transform.position = Vector3.zero;
+    }
+
+    private void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+        _rigidbody.velocity = Direction;
+        StartCoroutine(Fixed());
+    }
+
+    private IEnumerator Fixed()
+    {
+        var condition = new WaitUntil(() => MaxDistance <= gameObject.transform.position.magnitude);
+        while (true)
+        {
+            yield return condition;
+            _rigidbody.velocity *= -1;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
+
